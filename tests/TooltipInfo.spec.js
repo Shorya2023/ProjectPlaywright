50ea1966aca5063da2ea8789c22158c37764f1ba
Shorya2023 <beohar77@gmail.com>
2025-02-22 18:07:22 +0530

    tooltip updated

---
+++ tests/TooltipInfo.spec.js
@@ -0,0 +1,19 @@
+import {test,expect} from '@playwright/test'
+
+test("too tip veriication ", async({page})=>{
+
+   await page.goto("https://jqueryui.com/tooltip/");
+   await page.locator("#age").hover();
+   const value  = page.locator("#age").getAttribute('title')
+   console.log(value);
+   console.log(value);
+
+   
+   expect(value).toBe('We ask for your age only for statistical purposes.');
+    
+
+   console.log("code has been added to from main");
+   
+
+})
+
